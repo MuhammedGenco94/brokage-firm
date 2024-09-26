0ac53ed2343bc9c2f8c34f90d1fa0505e1afbf4d
MUGENCO <evatro.genco@mercedes-benz.com>
2024-09-27 00:09:16 +0300

    Init commit

---
+++ src/main/java/com/ing/brokagefirm/BrokageFirmApplication.java
@@ -0,0 +1,13 @@
+package com.ing.brokagefirm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrokageFirmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrokageFirmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ing/brokagefirm/BrokageFirmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ing.brokagefirm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrokageFirmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
