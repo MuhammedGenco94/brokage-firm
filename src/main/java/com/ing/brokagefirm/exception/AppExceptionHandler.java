8719e7bf67dd1e5f8a0e3d68967bcc8feb4538ba
MUGENCO <evatro.genco@mercedes-benz.com>
2024-09-29 21:28:51 +0300

    Global Exception Handler

---
+++ src/main/java/com/ing/brokagefirm/exception/AppExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.ing.brokagefirm.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.HibernateException;
+import org.postgresql.util.PSQLException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.sql.SQLException;
+
+@ControllerAdvice
+@Slf4j
+public class AppExceptionHandler {
+
+    @ExceptionHandler({HibernateException.class, SQLException.class, PSQLException.class})
+    @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
+    public void handleSQLExceptions(HttpServletRequest req, Exception ex) {
+        log.error("Error: {} \nFor Request URL: {}", ex.getMessage(), req.getRequestURL().toString());
+    }
+
+}
