22002479c427680627a739e4607a71969db66c29
MUGENCO <evatro.genco@mercedes-benz.com>
2024-09-29 23:20:04 +0300

    Customer logging

--- src/main/java/com/ing/brokagefirm/controller/CustomerController.java
+++ src/main/java/com/ing/brokagefirm/controller/CustomerController.java
@@ -2,6 +2,7 @@ package com.ing.brokagefirm.controller;
 
 import com.ing.brokagefirm.model.dto.CustomerDTO;
 import com.ing.brokagefirm.service.CustomerService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/customers")
+@Slf4j
 public class CustomerController {
 
     private final CustomerService customerService;
@@ -21,7 +23,9 @@ public class CustomerController {
     @PostMapping("/register")
     public ResponseEntity<String> registerCustomer(@RequestBody CustomerDTO customerDTO) {
         customerService.registerCustomer(customerDTO);
-        return ResponseEntity.ok("Customer \"" + customerDTO.getName() + "\" registered successfully");
+
+        log.info("Customer '{}' registered successfully", customerDTO.getName());
+        return ResponseEntity.ok("Customer '" + customerDTO.getName() + "' registered successfully");
     }
 
 }
