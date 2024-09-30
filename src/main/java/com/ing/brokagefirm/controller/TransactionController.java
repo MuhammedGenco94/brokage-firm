95aa94b26caf3941739d27a60ad8f600eb2ab238
MUGENCO <evatro.genco@mercedes-benz.com>
2024-09-30 04:35:49 +0300

    Init Transactions Controller

---
+++ src/main/java/com/ing/brokagefirm/controller/TransactionController.java
@@ -0,0 +1,11 @@
+package com.ing.brokagefirm.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/transactions")
+public class TransactionController {
+
+
+}
