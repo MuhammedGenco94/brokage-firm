921f653ab8d2347111503e6b90ea68a25a0d8f81
MUGENCO <evatro.genco@mercedes-benz.com>
2024-09-30 04:15:00 +0300

    Docs

--- src/main/java/com/ing/brokagefirm/service/AssetService.java
+++ src/main/java/com/ing/brokagefirm/service/AssetService.java
@@ -14,15 +14,31 @@ public class AssetService {
         this.assetRepository = assetRepository;
     }
 
+    /**
+     * List assets of a customer.
+     *
+     * @param customerId customer id
+     * @return list of assets
+     */
     public List<Asset> listAssets(Long customerId) {
         return assetRepository.findByCustomerId(customerId);
     }
 
+    /**
+     * Find asset by customer id and asset name.
+     *
+     * @param customerId customer id
+     * @param assetName  asset name
+     * @return asset
+     */
     public Asset findByCustomerIdAndAssetName(Long customerId, String assetName) {
         return assetRepository.findByCustomerIdAndAssetName(customerId, assetName)
                 .orElse(null);
     }
 
+    /**
+     * Save asset.
+     */
     public void save(Asset asset) {
         assetRepository.save(asset);
     }
